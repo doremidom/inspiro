3fb4a9550f4b47875498c6b5f7921928915bd0dd
David Su <david.d.su@gmail.com>
2015-07-10 17:38:04 -0400

    initial taylor swift commit; jquerymobile setup; initial mysql setup

---
+++ login.php
@@ -0,0 +1,60 @@
+<?php
+
+header('Content-type: application/json');
+// header('Access-Control-Allow-Origin, Access-Control-Allow-Methods and Access-Control-Allow-Headers');
+if($_POST) {
+	$username   = $_POST['username'];
+	$password   = $_POST['password'];
+
+	if($username && $password) {
+
+			$db_name     = 'NAME';
+			$db_user     = 'USER';
+			$db_password = 'PASSWORD';
+			$server_url  = 'URL';
+
+			$mysqli = new mysqli($server_url, $db_user, $db_password, $db_name);
+
+			/* check connection */
+			if (mysqli_connect_errno()) {
+				error_log("Connect failed: " . mysqli_connect_error());
+				echo '{"success":0,"error_message":"' . mysqli_connect_error() . '"}';
+			} else {
+				if ($stmt = $mysqli->prepare("SELECT username FROM users WHERE username = ? and password = ?")) {
+
+					$password = md5($password);
+
+					/* bind parameters for markers */
+					$stmt->bind_param("ss", $username, $password);
+
+					/* execute query */
+					$stmt->execute();
+
+					/* bind result variables */
+					$stmt->bind_result($id);
+
+					/* fetch value */
+					$stmt->fetch();
+
+					/* close statement */
+					$stmt->close();
+				}
+
+				/* close connection */
+				$mysqli->close();
+
+				if ($id) {
+					error_log("User $username: password match.");
+					echo '{"success":1}';
+				} else {
+					error_log("User $username: password doesn't match.");
+					echo '{"success":0,"error_message":"Invalid username/password combination"}';
+				}
+			}
+	} else {
+		echo '{"success":0,"error_message":"Invalid username/password combination"}';
+	}
+}else {
+	echo '{"success":0,"error_message":"Invalid data :("}';
+}
+?>
---
+++ signup.php
@@ -0,0 +1,61 @@
+<?php
+
+header('Content-type: application/json');
+if($_POST) {
+	$username   = $_POST['username'];
+	$password   = $_POST['password'];
+	$email		= $_POST['email'];
+	$fullname	= $_POST['fullname'];
+
+	// $c_password = $_POST['c_password'];
+
+	if($_POST['username']) {
+		// if ( $password == $c_password ) {
+
+			$db_name     = 'NAME';
+			$db_user     = 'USER';
+			$db_password = 'PASSWORD';
+			$server_url  = 'URL';
+
+			$mysqli = new mysqli($server_url, $db_user, $db_password, $db_name);
+
+			/* check connection */
+			if (mysqli_connect_errno()) {
+				error_log("Connect failed: " . mysqli_connect_error());
+				echo '{"success":0,"error_message":"' . mysqli_connect_error() . '"}';
+			} else {
+				$stmt = $mysqli->prepare("INSERT INTO users (username, password, email, fullname) VALUES (?, ?, ?, ?)");
+				$password = md5($password);
+				$stmt->bind_param('ss', $username, $password, $email, $fullname);
+
+				/* execute prepared statement */
+				$stmt->execute();
+
+				if ($stmt->error) {error_log("Error: " . $stmt->error); }
+
+				$success = $stmt->affected_rows;
+
+				/* close statement and connection */
+				$stmt->close();
+
+				/* close connection */
+				$mysqli->close();
+				error_log("Success: $success");
+
+				if ($success > 0) {
+					error_log("User '$username' created.");
+					echo '{"success":1}';
+				} else {
+					echo '{"success":0,"error_message":"Username exists."}';
+				}
+			}
+		// } else {
+		// 	echo '{"success":0,"error_message":"Passwords does not match."}';
+		// }
+	} else {
+		echo '{"success":0,"error_message":"Invalid username."}';
+	}
+}else {
+	echo '{"success":0,"error_message":"Invalid data :("}';
+}
+?>
