2214ee01ad2605f400b347cb6a8d55b53b131584
doremidom <dominiquexstar@gmail.com>
2015-10-16 14:47:13 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1"> 
         <meta name="apple-mobile-web-app-capable" content="yes" />
 
-        <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Roboto:700,300' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="style.css" />
 
         <link rel="stylesheet" href="themes/inspiro.min.css" />
